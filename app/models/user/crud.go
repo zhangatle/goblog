bbde0bc5cf6fc64fd2be0012c6769eec33ef04a1
zhangatle <zhangatle@gmail.com>
2023-03-17 14:11:11 +0800

    会话和认证管理

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -3,6 +3,7 @@ package user
 import (
 	"goblog/logger"
 	"goblog/model"
+	"goblog/types"
 )
 
 func (user *User) Create() (err error) {
@@ -11,4 +12,23 @@ func (user *User) Create() (err error) {
 		return err
 	}
 	return nil
+}
+
+// Get 通过 ID 获取用户
+func Get(idstr string) (User, error) {
+	var user User
+	id := types.StringToUint64(idstr)
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// GetByEmail 通过 Email 来获取用户
+func GetByEmail(email string) (User, error) {
+	var user User
+	if err := model.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
 }
